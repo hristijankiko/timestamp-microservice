1a0fb0fe91e4d1ef44b343a2e91e56ce0d48414c
Hristijan Gjorgjievski <Hristijan Gjorgjievski>
2016-11-30 15:25:23 +0100

    Server port now uses env variable

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/:time', function(req, res){
     res.json(timeStamp)
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("App is listening on port 3000!");
 });
 
